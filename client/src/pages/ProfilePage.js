1998ad118cea65ec2a8d8d7f6fcaeff95d2bd797
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-09-07 23:34:03 -0400

    finish profile page css for now

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 import { fetchQuestions } from '../store/questions';
 import { Redirect } from 'react-router-dom';
 import '../profilePage.css';
+import stockStackOverflowProPic from '../images/stockStackOverflowProPic.png';
 
 function ProfilePage() {
   const username = useSelector(state => state.auth.username);
@@ -27,23 +28,33 @@ function ProfilePage() {
 
   return (
     <div id='container'>
-      <p>Under Construction</p>
+      <div id='left-side-bar'>
+        <p>Under Construction</p>
+      </div>
       {/* <h1>{`Hello ${username}! This is a working test!!!`}</h1> */}
-      <button onClick={handleLogOut}>Log Out</button>
-      <button onClick={toMainPage}>Main Page</button>
+      <div id='user-info-container'>
+        <div id='user-info'>
+          <div id='user-picture'>
+            <img src={stockStackOverflowProPic} />
+          </div>
+          <div>
+            <h2>{username}</h2>
+            <div>Student at App Academy</div>
+            <div>(Your about me is currently blank.)</div>
+            <div>About me editing currently under construction.</div>
+          </div>
+        </div>
+        <div id='user-helpful-links'>
+          <h3>Helpful user links and answered questions coming soon!</h3>
+        </div>
+      </div>
+      <div id='user-stats'>
+        User Statistics Coming Soon!
+      </div>
+      {/* <button onClick={handleLogOut}>Log Out</button>
+      <button onClick={toMainPage}>Main Page</button> */}
     </div>
   );
 }
 
 export default ProfilePage;
-// To delete a cookie, use the clearCookie function. For example, if you need to clear a cookie named foo, use the following code.
-
-// var express = require('express');
-// var app = express();
-
-// app.get('/clear_cookie_foo', function(req, res){
-//    res.clearCookie('foo');
-//    res.send('cookie foo cleared');
-// });
-
-// app.listen(3000);
